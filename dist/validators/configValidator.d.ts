8db1c1d5d02a5919ddd136bbf810664fb856426a
Miceprincipal <114647280+Miceprincipal@users.noreply.github.com>
2025-10-03 23:35:30 +0100

    Delete dist directory
    whoops

--- dist/validators/configValidator.d.ts
+++
@@ -1,9 +0,0 @@
-import { GeneratorConfig } from '../types/config';
-export declare class ConfigValidator {
-    private schema;
-    constructor();
-    validate(config: any): GeneratorConfig;
-    validatePartial(config: any, section: string): any;
-    createDefaultConfig(): GeneratorConfig;
-}
-//# sourceMappingURL=configValidator.d.ts.map
